c57ffd1bed9862ec4bf7b94c6ab295f86bb93700
rajcarthy <rajcarthy@gmail.com>
2021-03-03 16:02:16 -0600

    sample react app

---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+
+function HomePage() {
+    return(
+    <div className="jumbotron">
+        <h1>Book Administration</h1>
+        <p> Books you have read</p>
+        <a href ="/about">About</a> 
+    </div>
+    );
+    
+}
+
+export default HomePage; 
+
